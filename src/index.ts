15a7eba0d644c75e0c9fd950d1312e98c598a50b
Komyagin Alexey <komyagin.alexey@gmail.com>
2020-04-10 18:01:23 +0500

    hover-car class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {Engine, Scene, StandardMaterial, ShadowGenerator, FreeCamera, Vector3, A
 import {GridMaterial} from 'babylonjs-materials';
 
 import "babylonjs-loaders";
+import { HoverCar } from './models/hover-car';
 
 
 let canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
@@ -28,15 +29,15 @@ var light = new DirectionalLight("dir01", new Vector3(-1, -1, -1), scene);
     light.position = new Vector3(20, 40, 20);
     light.intensity = 0.5
 
-    var shadowGenerator = new ShadowGenerator(1024, light);
-    
-    let ground = Mesh.CreateGround("ground1", 100, 100, 2, scene);
-    // let material = new StandardMaterial("grid", scene);
-    let material = new GridMaterial("grid", scene);
-    // material.diffuseColor = new BABYLON.Color3(0,1,0)   
-    ground.material = material;
-    ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9, friction: 1 }, scene);
-    ground.receiveShadows = true;
+var shadowGenerator = new ShadowGenerator(1024, light);
+
+let ground = Mesh.CreateGround("ground1", 100, 100, 2, scene);
+// let material = new StandardMaterial("grid", scene);
+let material = new GridMaterial("grid", scene);
+// material.diffuseColor = new BABYLON.Color3(0,1,0)   
+ground.material = material;
+ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9, friction: 1 }, scene);
+ground.receiveShadows = true;
 
 // Keyboard events
 var inputMap: {[k: string]: boolean} = {};
@@ -48,293 +49,24 @@ scene.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnKeyUpTr
     inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
 }));
 
-// let sphere = MeshBuilder.CreateSphere("sphere", {diameterX: 1, diameterY: 1, diameterZ: 1}, scene);
-// sphere.physicsImpostor = new PhysicsImpostor(sphere, PhysicsImpostor.SphereImpostor, {mass:1}, scene);
-// sphere.position.y = 1;
-
-var box = MeshBuilder.CreateBox("Collider", {width: 1, height: 0.5, depth: 2}, scene);
-var box1 = MeshBuilder.CreateBox("fakeHoverEngineFR", {size:0.25}, scene);
-var box2 = MeshBuilder.CreateBox("fakeHoverEngineBR", {size:0.25}, scene);
-var box3 = MeshBuilder.CreateBox("fakeHoverEngineBL", {size:0.25}, scene);
-var box4 = MeshBuilder.CreateBox("fakeHoverEngineFL", {size:0.25}, scene);
-var box5 = MeshBuilder.CreateBox("fakeHoverEngineCameraTarget", {size:0.25}, scene);
-
-let root = new Mesh('', scene);
-shadowGenerator.addShadowCaster(box);
-
-
-root.addChild(box);
-root.addChild(box1);
-root.addChild(box2);
-root.addChild(box3);
-root.addChild(box4);
-root.addChild(box5);
-
-box1.position = new Vector3(0.5, 0, 1);
-box2.position = new Vector3(0.5, 0, -1)
-box3.position = new Vector3(-0.5, 0, -1);
-box4.position = new Vector3(-0.5, 0, 1);
-
-
-
-var matBox = new StandardMaterial("matBox", scene);
-matBox.diffuseColor = new BABYLON.Color3(1.0, 0.1, 0.1);
-
-box.material = matBox
-
-var matBox1 = new StandardMaterial("matBox", scene);
- matBox1.diffuseColor = new BABYLON.Color3(0, 0, 1)
- box1.material = matBox1;
- box4.material = matBox1;
-
- let needStabilize = true;
-
  let obsticleBox = MeshBuilder.CreateBox('obstB1', {width: 10, height: 1, depth: 1}, scene);
  obsticleBox.physicsImpostor = new PhysicsImpostor(obsticleBox, PhysicsImpostor.BoxImpostor, {mass: 1, friction: 1}, scene);
  obsticleBox.position = new Vector3(0, 0.5, 2);
+
+ let hoverCar = new HoverCar(scene);
+ hoverCar.position = new Vector3(0,1,0);
+ camera.lockedTarget = hoverCar.body;
+ console.log(hoverCar.body.position, hoverCar.body.getAbsolutePosition())
+ hoverCar.initControls();
+
+ hoverCar.enableHoverEngines();
 
 
  engine.runRenderLoop(() => {
     scene.render();
 });
 
-// let hovercarTask = assetsManager.addMeshTask('hoverCar task', '', 'assets/', 'hover-car.gltf');
-// hovercarTask.onSuccess =t => {
-    // console.log(t.loadedMeshes)
-    // let newMeshes = t.loadedMeshes;
-    // var physicsRoot = new Mesh("physicsRoot", scene);
-    var physicsRoot = root;
-    // let car = t.loadedMeshes[0];
-    let hoverEngines: AbstractMesh[] = [box1, box2, box3, box4];
-    let frHover: AbstractMesh | null = box1;
-    let flHover: AbstractMesh | null = box4;
-    let brHover: AbstractMesh | null = box2;
-    let blHover: AbstractMesh | null = box3;
-
-    camera.lockedTarget = box5;
-
-    // t.loadedMeshes[0].rotation = new Vector3(0, 0, 0);
-
-    // newMeshes.forEach((m, i)=>{
-    //     if(m.name.indexOf("Collider") != -1){
-    //         m.isVisible = false
-    //         physicsRoot.addChild(m)
-    //     }
-
-    //     if(m.name.includes('fakeHoverEngine')) {
-    //         hoverEngines.push(m);
-    //         if (m.name.includes('FR'))
-    //             frHover = m;
-    //         else if (m.name.includes('FL'))
-    //             flHover = m;
-    //         else if (m.name.includes('BL'))
-    //             blHover = m;
-    //         else if (m.name.includes('BR'))
-    //             brHover = m;
-    //         // m.isVisible = false;
-    //         // physicsRoot.addChild(m);
-    //         // m.physicsImpostor = new PhysicsImpostor(m, PhysicsImpostor.BoxImpostor, {mass: 0.1}, scene);
-    //     }
-
-    //     m.isPickable = false;
-    // })
-
-    // Add all root nodes within the loaded gltf to the physics root
-    // newMeshes.forEach((m, i)=>{
-    //     if(m.parent == null){
-    //         m.position = Vector3.Zero();
-    //         physicsRoot.addChild(m)
-    //     }
-    // })
-
-    // Make every collider into a physics impostor
-    physicsRoot.getChildMeshes().forEach((m)=>{
-        m.isPickable = false;
-        if(m.name.indexOf("Collider") != -1){
-            m.scaling.x = Math.abs(m.scaling.x)
-            m.scaling.y = Math.abs(m.scaling.y)
-            m.scaling.z = Math.abs(m.scaling.z)
-            m.physicsImpostor = new PhysicsImpostor(m, PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
-        }
-    })
-    
-    physicsRoot.physicsImpostor = new PhysicsImpostor(physicsRoot, PhysicsImpostor.NoImpostor, { mass: 100, friction: 1 }, scene);
-    physicsRoot.position = new Vector3(0, 2, 0);
-
-    function castRay(mesh: AbstractMesh){
-        var origin = mesh.getAbsolutePosition();
-
-	    var forward = new Vector3(0,-1,0);
-	    forward = Vector3.TransformCoordinates(forward, mesh.getWorldMatrix());
-
-	    var direction = forward.subtract(origin);
-	    direction = Vector3.Normalize(direction);
-
-	    var length = 7;
-
-	    var ray = new Ray(origin, direction, length);
-
-		let rayHelper = new RayHelper(ray);
-		// rayHelper.show(scene);
-
-        var hit = scene.pickWithRay(ray);
-
-        if (hit && hit.hit && hit.pickedMesh && hit.pickedMesh.name !== 'ray'){
-            // console.log(hit.pickedMesh)
-
-           if (hit.distance < 2) {
-               pulse(mesh, hit.distance, 2);
-               needStabilize = true;
-           }
-
-
-           if (needStabilize) {
-            stabilize(mesh, physicsRoot, box)
-           }
-
-	    }
-    }
-
-    scene.registerBeforeRender(function () {
-        for (let he of hoverEngines) {
-            castRay(he);
-        }
-
-        let t = physicsRoot.physicsImpostor?.getAngularVelocity();
-        if (t) {
-            physicsRoot.physicsImpostor?.setAngularVelocity(t.scale(0.05))
-        }
-
-        //fake drag
-        let v = physicsRoot.physicsImpostor?.physicsBody ? physicsRoot.physicsImpostor?.getLinearVelocity() : null;
-        if (v) {
-            // console.log('pos', physicsRoot.getAbsolutePosition())
-            let length = v.length();
-            let normDir = v.normalizeToNew();
-            
-            normDir.negateInPlace()
-            let dragMagnitude = Math.pow(length, 2) * 10;
-            
-            physicsRoot.physicsImpostor?.applyForce(normDir.scale(dragMagnitude), box.getAbsolutePosition());
-            // var ray = new Ray(box.getAbsolutePosition(), normDir.scale(dragMagnitude), 20);
-
-            // let rayHelper = new RayHelper(ray);
-            // rayHelper.show(scene);
-
-            // setTimeout(() => rayHelper.dispose(), 500)
-        }
-            
-    });
-
-
-    var contactLocalRefPoint = Vector3.Zero();
-
-    function pulse(mesh: AbstractMesh, distance: number, max: number) {
-
-
-        var origin = mesh.getAbsolutePosition();
-
-	    var forward = new Vector3(0,1,0);
-	    forward = Vector3.TransformCoordinates(forward, mesh.getWorldMatrix());
-
-	    var direction = forward.subtract(origin);
-	    direction = Vector3.Normalize(direction);
-
-        let formula = 1 - distance / max;
-
-        let forceMagnitude = formula * 900;
-
-        physicsRoot.physicsImpostor?.applyForce(direction.scale(forceMagnitude), mesh.getAbsolutePosition());
-
-    }
-
-    scene.onBeforeRenderObservable.add(()=>{
-        var physicsRootOrigin = physicsRoot.getAbsolutePosition();
-        let touque = 40;
-        let f = 30
-
-        if(inputMap["w"] || inputMap["ArrowUp"]){
-            
-            physicsRoot.physicsImpostor?.applyImpulse(
-                Vector3.Normalize(Vector3.TransformCoordinates(Vector3.Forward(), box.getWorldMatrix()).subtract(box.getAbsolutePosition())).scale(f),
-                box.getAbsolutePosition()
-            )
-        } 
-        if((inputMap["a"] || inputMap["ArrowLeft"]) && frHover && blHover && physicsRoot){
-
-            let fdirection = Vector3.Normalize(vecToLocal(Vector3.Left(), frHover).subtract(physicsRoot.getAbsolutePosition()));
-            let bdirection = Vector3.Normalize(vecToLocal(Vector3.Right(), blHover).subtract(physicsRoot.getAbsolutePosition()));
-            
-            physicsRoot.physicsImpostor?.applyImpulse(
-                fdirection.scale(touque),
-                frHover.getAbsolutePosition()
-            );
-            physicsRoot.physicsImpostor?.applyImpulse(
-                bdirection.scale(touque),
-                blHover.getAbsolutePosition()
-            )
-        } 
-        if(inputMap["s"] || inputMap["ArrowDown"]){
-            physicsRoot.physicsImpostor?.applyImpulse(
-                Vector3.Normalize(Vector3.TransformCoordinates(Vector3.Backward(), box.getWorldMatrix()).subtract(box.getAbsolutePosition())).scale(f),
-                box.getAbsolutePosition()
-            )
-        } 
-        if((inputMap["d"] || inputMap["ArrowRight"]) && flHover && brHover){
-            let fdirection = Vector3.Normalize(vecToLocal(Vector3.Right(), flHover).subtract(physicsRootOrigin));
-            let bdirection = Vector3.Normalize(vecToLocal(Vector3.Left(), brHover).subtract(physicsRootOrigin));
-    
-            physicsRoot.physicsImpostor?.applyImpulse(
-                fdirection.scale(touque),
-                flHover.getAbsolutePosition()
-            );
-            physicsRoot.physicsImpostor?.applyImpulse(
-                bdirection.scale(touque),
-                brHover.getAbsolutePosition()
-            )
-        }
-        
-        if(inputMap[" "]){
-            needStabilize = false;
-            physicsRoot.physicsImpostor?.applyImpulse(
-                Vector3.Normalize(
-                    Vector3.TransformCoordinates(Vector3.Up(), physicsRoot.getWorldMatrix()).subtract(physicsRootOrigin)).scale(50),
-                physicsRootOrigin
-            )
-        }
-    })
-    
-// }
-
-assetsManager.load();
 
 function degToRad(degree: number) {
     return degree / (180 / Math.PI);
-}
-
-function vecToLocal(vector: Vector3, mesh: AbstractMesh){
-    var m = mesh.getWorldMatrix();
-    var v = Vector3.TransformCoordinates(vector, m);
-    return v;		 
-}
-
-function stabilize(he: AbstractMesh, phRoot: AbstractMesh, box: AbstractMesh) {
-    
-    let v = phRoot.physicsImpostor?.physicsBody ? phRoot.physicsImpostor?.getLinearVelocity() : null;
-
-    if (v) {
-        // console.log('pos', physicsRoot.getAbsolutePosition())
-        let length = v.y;
-        
-        let dragMagnitude = new Vector3(0, -v.y, 0).scale(Math.abs(length) * 80);
-        
-        phRoot.physicsImpostor?.applyForce(dragMagnitude, box.getAbsolutePosition());
-        // var ray = new Ray(box.getAbsolutePosition(), normDir.scale(dragMagnitude), 20);
-
-        // let rayHelper = new RayHelper(ray);		
-        // rayHelper.show(scene);
-
-        // setTimeout(() => rayHelper.dispose(), 500)
-    }
-    
 }
---
+++ src/models/hover-car.ts
@@ -0,0 +1,275 @@
+import { Scene, PhysicsImpostor, AbstractMesh, Mesh, MeshBuilder, Vector3, StandardMaterial, Ray, ActionManager, ExecuteCodeAction } from "babylonjs";
+
+
+
+export class HoverCar {
+
+    static hoverHeight: number = 2;
+    static thrustMagnitude: number = 40;
+    static torqueMagnitude: number = 8;
+    static jumpMagnitude: number = 200;
+
+    private _physicsRoot: AbstractMesh;
+    private _scene: Scene;
+    private _hoverEngineFR: AbstractMesh;
+    private _hoverEngineFL: AbstractMesh;
+    private _hoverEngineBR: AbstractMesh;
+    private _hoverEngineBL: AbstractMesh;
+    private _body: AbstractMesh;
+    private _inputMap: {[k: string]: boolean} = {};
+
+    private _stabilizationEnabled: boolean = false;
+
+    get body() {
+        return this._body;
+    }
+
+    get position() {
+        return this._physicsRoot.position;
+    }
+
+    set position(position: Vector3) {
+        this._physicsRoot.position = position;
+    }
+
+    constructor(scene: Scene) {
+        this._scene = scene;
+        
+        
+        //materials
+        let carBodyM = new StandardMaterial("matBox", this._scene);
+        carBodyM.diffuseColor = new BABYLON.Color3(1.0, 0.1, 0.1);
+        let frontHoverEngineM = new StandardMaterial("matBox", this._scene);
+        frontHoverEngineM.diffuseColor = new BABYLON.Color3(0, 0, 1)
+
+        // meshes
+
+        this._physicsRoot = new Mesh('p_root', this._scene);
+        this._body = new Mesh('body_root', this._scene);
+        
+        let carBody = MeshBuilder.CreateBox("body", {width: 1, height: 0.5, depth: 2}, this._scene);
+        carBody.material = carBodyM;
+        
+        this._hoverEngineFR = MeshBuilder.CreateBox("hoverEngineFR", {size:0.25}, this._scene);
+        this._hoverEngineFL = MeshBuilder.CreateBox("hoverEngineFL", {size:0.25}, this._scene);
+        this._hoverEngineBR = MeshBuilder.CreateBox("hoverEngineBR", {size:0.25}, this._scene);
+        this._hoverEngineBL = MeshBuilder.CreateBox("hoverEngineBL", {size:0.25}, this._scene);
+        
+        
+        let collider = MeshBuilder.CreateBox("collider", {width: 1, height: 0.5, depth: 2}, this._scene);
+        collider.physicsImpostor = new PhysicsImpostor(
+            collider,
+            PhysicsImpostor.BoxImpostor,
+            {mass: 0},
+            this._scene
+        )
+        collider.isVisible = false;
+
+        
+        this._hoverEngineFR.material = frontHoverEngineM;
+        this._hoverEngineFL.material = frontHoverEngineM;
+
+        //hierarchy
+        this._physicsRoot.addChild(collider);
+        this._physicsRoot.addChild(this._body);
+
+        this._body.addChild(carBody);
+        this._body.addChild(this._hoverEngineFR);
+        this._body.addChild(this._hoverEngineFL);
+        this._body.addChild(this._hoverEngineBR);
+        this._body.addChild(this._hoverEngineBL);
+        
+        this._body.isPickable = false;
+        this._body.getChildMeshes().forEach(m => m.isPickable = false);
+
+        this._hoverEngineFR.position = new Vector3(0.5, 0, 1);
+        this._hoverEngineFL.position = new Vector3(-0.5, 0, 1);
+        this._hoverEngineBR.position = new Vector3(0.5, 0, -1);
+        this._hoverEngineBL.position = new Vector3(-0.5, 0, -1);
+
+
+
+        this._physicsRoot.physicsImpostor = new PhysicsImpostor(
+            this._physicsRoot,
+            PhysicsImpostor.NoImpostor,
+            { mass: 100, friction: 1 },
+            this._scene
+        )
+
+        
+
+    }
+
+    private activateAngularDrag() {
+        this._scene.registerBeforeRender(() => {
+            let v = this._physicsRoot.physicsImpostor?.getAngularVelocity();
+        
+            if (v) {
+                this._physicsRoot.physicsImpostor?.setAngularVelocity(v.scale(0.9))
+            }
+        })
+    }
+
+    private activateDrag() {
+        this._scene.registerBeforeRender(() => {
+            let v = this._physicsRoot.physicsImpostor?.getLinearVelocity();
+        
+            if (v) {
+                let length = v.length();
+                let normDir = v.normalizeToNew();
+                
+                normDir.negateInPlace()
+                let dragMagnitude = Math.pow(length, 2) * 10;
+                
+                this._physicsRoot.physicsImpostor?.applyForce(normDir.scale(dragMagnitude), this._body.getAbsolutePosition());
+            }
+        })
+    }
+
+    public enableHoverEngines() {
+
+        this.activateAngularDrag();
+        this.activateDrag();
+
+        let engines = [
+            this._hoverEngineFR,
+            this._hoverEngineFL,
+            this._hoverEngineBR,
+            this._hoverEngineBL
+        ];
+
+        this._stabilizationEnabled = true;
+
+        this._scene.registerBeforeRender(() => {
+            for (let engine of engines) {
+                let ray = this.castRayFromEngine(engine);
+                let pickingInfo = this._scene.pickWithRay(ray);
+    
+                if (pickingInfo 
+                    && pickingInfo.hit 
+                    && pickingInfo.pickedMesh?.name !== 'ray' // in case of showing rays
+                ) {
+                    if (pickingInfo.distance < HoverCar.hoverHeight) {
+                        this.push(engine, pickingInfo.distance);
+                    }
+    
+                    if (this._stabilizationEnabled) {
+                        this.stabilize();
+                    }
+                }
+    
+            }
+        })
+
+        // this.activateAngularDrag();
+    }
+
+    private castRayFromEngine(engine: AbstractMesh) {
+        let origin = engine.getAbsolutePosition();
+        let direction = Vector3.Normalize(
+            vecToLocal(Vector3.Down(), engine).subtract(origin),
+        );
+        let length = HoverCar.hoverHeight * 1.5;
+
+        return new Ray(origin, direction, length);
+    }
+
+    private push(hoverEngine: AbstractMesh, distance: number) {
+        let contactPoint = hoverEngine.getAbsolutePosition();
+        let direction = Vector3.Normalize(
+            vecToLocal(Vector3.Up(), hoverEngine).subtract(contactPoint)
+        );
+        let forceMagnitude = (1 - distance / HoverCar.hoverHeight) * 900;
+        let force = direction.scale(forceMagnitude);
+
+        this._physicsRoot.physicsImpostor?.applyForce(force, contactPoint);
+    }
+
+    private stabilize() {
+        let v = this._physicsRoot.physicsImpostor?.getLinearVelocity();
+        let contactPoint = this._body.getAbsolutePosition();
+    
+        if (v) {
+            let magnitude = Math.abs(v.y) * 80;
+            let force = new Vector3(0, -v.y, 0).scale(magnitude);
+
+            this._physicsRoot.physicsImpostor?.applyForce(force, contactPoint);
+        }
+    }
+
+    private thrust(direction: Vector3) {
+        let contactPoint = this._body.getAbsolutePosition();
+        
+        direction.normalize();
+        let force = vecToLocal(direction, this._body)
+            .subtract(contactPoint)
+            .scale(HoverCar.thrustMagnitude)
+
+        this._physicsRoot.applyImpulse(force, contactPoint)
+    }
+
+    private torque(clockWise: boolean) {
+        let hoverEngineF = clockWise ? this._hoverEngineFL : this._hoverEngineFR;
+        let hoverEngineB = clockWise ? this._hoverEngineBR : this._hoverEngineBL; 
+        let fDirection = clockWise ? Vector3.Right() : Vector3.Left();
+        let bDirection = fDirection.negate();
+
+        let fForce = Vector3.Normalize(
+            vecToLocal(fDirection, hoverEngineF).subtract(this._body.getAbsolutePosition())
+        ).scale(HoverCar.torqueMagnitude)
+
+        let bForce = Vector3.Normalize(
+            vecToLocal(bDirection, hoverEngineB).subtract(this._body.getAbsolutePosition())
+        ).scale(HoverCar.torqueMagnitude)
+
+        this._physicsRoot.physicsImpostor?.applyImpulse(fForce, hoverEngineF.getAbsolutePosition())
+        this._physicsRoot.physicsImpostor?.applyImpulse(bForce, hoverEngineB.getAbsolutePosition())
+    }
+
+    private jump() {
+        this._stabilizationEnabled = false;
+        setTimeout(() => this._stabilizationEnabled = true, 300);
+        this._physicsRoot.physicsImpostor?.applyImpulse(
+            Vector3.Normalize(vecToLocal(Vector3.Up(), this._body).subtract(this._body.getAbsolutePosition())).scale(HoverCar.jumpMagnitude),
+            this._body.getAbsolutePosition()
+        )
+    }
+
+    public initControls() {
+        this._scene.actionManager = new ActionManager(this._scene);
+        
+        this._scene.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (evt) => {								
+            this._inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+        }));
+
+        this._scene.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (evt) => {								
+            this._inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+        }));
+
+        this._scene.onBeforeRenderObservable.add(()=>{
+    
+            if(this._inputMap["w"] || this._inputMap["ArrowUp"]){
+                this.thrust(Vector3.Forward());
+            } 
+            if((this._inputMap["a"] || this._inputMap["ArrowLeft"])){
+                this.torque(false)
+            } 
+            if(this._inputMap["s"] || this._inputMap["ArrowDown"]){
+                this.thrust(Vector3.Backward());
+            } 
+            if((this._inputMap["d"] || this._inputMap["ArrowRight"])){
+                this.torque(true);
+            }
+            if(this._inputMap[" "]){
+                this.jump();
+            }
+        })
+    }
+
+    
+}
+
+
+function vecToLocal(vec: Vector3, mesh: AbstractMesh) {
+    return Vector3.TransformCoordinates(vec, mesh.getWorldMatrix());
+}
